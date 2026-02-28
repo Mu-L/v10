ed43d52f8fa347acb9dd62f8458a8324e3506fd1
rahim <rahim.alwer@gmail.com>
2026-02-28 00:40:03 -0800

    feat(react): add popover component (#653)

---
+++ packages/react/src/ui/popover/popover-arrow.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { PopoverDataAttrs, type PopoverState } from '@videojs/core';
+import { forwardRef } from 'react';
+
+import type { UIComponentProps } from '../../utils/types';
+import { renderElement } from '../../utils/use-render';
+import { usePopoverContext } from './popover-context';
+
+export interface PopoverArrowProps extends UIComponentProps<'div', PopoverState> {}
+
+export const PopoverArrow = forwardRef<HTMLDivElement, PopoverArrowProps>(function PopoverArrow(
+  { render, className, style, ...elementProps },
+  forwardedRef
+) {
+  const { state } = usePopoverContext();
+
+  return renderElement(
+    'div',
+    { render, className, style },
+    {
+      state,
+      stateAttrMap: PopoverDataAttrs,
+      ref: forwardedRef,
+      props: [
+        {
+          'aria-hidden': 'true' as const,
+        },
+        elementProps,
+      ],
+    }
+  );
+});
+
+export namespace PopoverArrow {
+  export type Props = PopoverArrowProps;
+  export type State = PopoverState;
+}
