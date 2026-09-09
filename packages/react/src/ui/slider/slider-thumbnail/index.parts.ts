844902ac800d58a0aad750f1c3a1c24ca7328929
rahim <rahim.alwer@gmail.com>
2026-09-08 23:17:06 -0700

    refactor(react): use simple file names in ui directories (#2674)

--- packages/react/src/ui/slider/slider-thumbnail/index.parts.ts
+++
@@ -1,2 +0,0 @@
-export { ThumbnailImage as Image, type ThumbnailImageProps as ImageProps } from '../../thumbnail/thumbnail-image';
-export { SliderThumbnailRoot as Root, type SliderThumbnailRootProps as RootProps } from './slider-thumbnail-root';
